3c9ee7ff07ff23b8e439895989c5f730946029ce
mstr07 <pyra.michal777@gmail.com>
2023-01-11 21:11:00 +0100

    Assets deleted

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    //TODO
+    public Slider slider;
+
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
 }
